d23a54a5df67108a1c629775498000cb3441f945
Athithyan61 <athithyanathithyan06@gmail.com>
2026-04-03 13:21:25 +0530

    First Commit 03

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+
+      <h1>Welcome to Picky Project! All the best for your project — give it your best and make it a success!</h1>
+    </div>
+  );
+}
